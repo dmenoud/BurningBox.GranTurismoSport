041cdce534c4e06227c26ddfa5ca50c041eeb111
David Menoud <davidmenoud@hotmail.com>
2018-05-10 19:00:55 +0200

    Add project files.

---
+++ BurningBox.GranTurismoSport.Strategie.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BurningBox.GranTurismoSport.Strategie.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
